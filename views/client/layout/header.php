f619d8f06009ca110b1a2c9347fd8f5bc35c088a
Jase Sous <jasesous@gmail.com>
2026-03-27 14:51:54 +0700

    fix(ui): edit tab title to VOGUE SHOP

--- views/client/layout/header.php
+++ views/client/layout/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>THE FASHION HOUSE - Monochrome Elegance</title>
+    <title>VOGUE SHOP</title>
     <link rel="stylesheet" href="./public/css/style.css">
 </head>
 <body>
